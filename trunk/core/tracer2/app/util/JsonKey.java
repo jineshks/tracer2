49e6108b14e378f8a12590ecdf7f9e8b3dd39ea5
manzarul07@gmail.com <manzarul07@gmail.com@257784d7-0a36-252b-5184-f2a686120711>
2013-10-03 12:01:27 +0000

    adding new utility classes.

---
+++ trunk/core/tracer2/app/util/JsonKey.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package util;
+
+/**
+ * @author Manzarul.Haque
+ * This class contains all json keys which will be used in the application.
+ *
+ */
+public class JsonKey {
+    /*
+    * USER_NAME contains userName as key
+    */
+    public static final String USER_ID = "userId";
+    /*
+     * USER_NAME contains userName as key
+     */
+	public static final String USER_NAME = "userName";
+	/*
+	 * PASSWORD contains password
+	 */
+	public static final String PASSWORD = "password";
+	/*
+	 *OLD_PASSWORD  
+	 */
+    public static final String OLD_PASSWORD = "oldPass";
+    /*
+     * NEW_PASSWORD
+     */
+    public static final String NEW_PASSWORD = "newPass";
+   
+    /*
+     * SESSION
+     */
+    public static final String SESSION ="sessionId";
+    /*
+     *FIRST_NAME 
+     */
+    public static final String FIRST_NAME = "firstName";
+    /*
+     * LAST_NAME
+     */
+    public static final String LAST_NAME = "lastName";
+   
+    /*
+     * PHONE_NUMBER
+     */
+    public static final String PHONE_NUMBER= "phoneNumber";
+   
+    public static final String STATUS_CODE  = "statusCode";
+    public static final String  STATUS_MESSAGE = "StatusMessage";
+    public static final String  ERROR_MESSAGE = "ErrorMessage";
+    public static final String  RESPONSE_DATE  ="ResponseData";
+}
