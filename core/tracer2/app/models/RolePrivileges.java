8da6302dde26bdb8394b70a63e32718087bc008f
manzarul07@gmail.com <manzarul07@gmail.com@257784d7-0a36-252b-5184-f2a686120711>
2013-10-03 12:03:05 +0000

    adding new bean classes.

---
+++ core/tracer2/app/models/RolePrivileges.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package models;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import play.db.ebean.Model;
+
+/*
+ * @author Manzarul.Haque
+ *
+ */
+@Entity
+@Table(name="role_privilege")
+public class RolePrivileges  extends Model{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	public long id;
+	@OneToOne
+	public Role role;
+
+	@OneToMany(cascade=CascadeType.ALL)
+	public List<Privileges> privileges ;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the role
+	 */
+	public Role getRole() {
+		return role;
+	}
+
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	/**
+	 * @return the privileges
+	 */
+	public List<Privileges> getPrivileges() {
+		return privileges;
+	}
+
+	/**
+	 * @param privileges the privileges to set
+	 */
+	public void setPrivileges(List<Privileges> privileges) {
+		this.privileges = privileges;
+	}
+ 	
+}
