fe327d224aff33d12b54c5b54a5e98ee9f08a7f5
manzarul07@gmail.com <manzarul07@gmail.com@257784d7-0a36-252b-5184-f2a686120711>
2014-01-03 06:56:28 +0000

    adding dao file.

---
+++ core/tracer2/app/dataAccess/dao/TestCaseDao.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package dataAccess.dao;
+
+import java.util.List;
+
+import responseBean.TestCaseResponse;
+
+import models.TestCase;
+
+/**
+ * @author Manzarul.Haque
+ *this interface contains all test case data base related 
+ *methods.
+ */
+public interface TestCaseDao {
+
+	/**
+	 * This method will create Test case.
+	 * @param testCase TestCase
+	 * @param phase int
+	 * @return boolean
+	 */
+	public boolean createTestCase(TestCase testCase,int phase);
+	/**
+	 * This method will provide list of test cases based on project.
+	 * @param projectId long
+	 * @return  List<TestCaseResponse>
+	 */
+	public List<TestCaseResponse> getTestCaseByProject(long projectId);
+	/**
+	 * This method will provide Test cases based on mile Stone
+	 * @param mileStoneId long
+	 * @return List<TestCaseResponse>
+	 */
+	public List<TestCaseResponse> getTestCaseByMileStone(long mileStoneId);
+	/**
+	 * This method will provide list of test cases based on ticket.
+	 * @param ticketId long
+	 * @return List<TestCaseResponse>
+	 */
+	public List<TestCaseResponse> getTestCaseByTicket(long ticketId);
+	/**
+	 * This method will provide test case based on user id.
+	 * @param userId long
+	 * @return List<TestCaseResponse>
+	 */
+	public List<TestCaseResponse> getTestCaseByUser(long userId);
+	
+	/**
+	 * This method will provide list of test cases based on project and user
+	 * @param projectId long
+	 * @param userId long
+	 * @return  List<TestCaseResponse>
+	 */
+	public List<TestCaseResponse> getTestCaseByProjectAndUser(long projectId,long userId);
+	
+	/**
+	 * This method will provide list of test cases based on mile stone and user
+	 * @param mileStone long
+	 * @param userId long
+	 * @return  List<TestCaseResponse>
+	 */
+	public List<TestCaseResponse> getTestCaseByUserAndMileStone(long mileStone,long userId);
+
+}
